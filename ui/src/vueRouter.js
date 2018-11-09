68677adf08409b4a0bc9207147890d01b5512b09
Kevin Glover <ktg@cs.nott.ac.uk>
2018-11-09 11:26:50 +0000

    Fix view path

--- ui/src/vueRouter.js
+++ ui/src/vueRouter.js
@@ -2,6 +2,7 @@ import VueRouter from "vue-router";
 
 import AppData from './pages/AppData.vue'
 import AppStore from './pages/AppStore.vue'
+import AppStoreToolbar from './pages/AppStoreToolbar.vue'
 import AppUI from './pages/AppUI.vue'
 import AppUIToolbar from './pages/AppUIToolbar.vue'
 import DataSource from './pages/DataSource.vue'
@@ -23,7 +24,12 @@ const router = new VueRouter({
 		{path: '/data/:app', component: AppData, props: true},
 		{path: '/datasource/:datasource', component: DataSource, props: true},
 		{path: '/login', component: Login},
-		{path: '/store', component: AppStore},
+		{path: '/store',  components: {
+				default: AppStore,
+				toolbar: AppStoreToolbar
+			},
+			props: {default: true, toolbar: false}
+		},
 		{path: '/settings', component: Settings},
 		{path: '/settings/system', component: SettingsSystem},
 		{path: '/install/:app', component: Install, props: true},
